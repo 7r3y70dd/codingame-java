da3b19a24533655b9f1f08a2810d653c12a2bf42
TR3Y <trey.todd@gmail.com>
2017-06-14 15:44:18 -0700

    yo

---
+++ src/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) throws FileNotFoundException {
+        Scanner in = new Scanner(args.length > 0 ? new FileInputStream(args[0]): System.in);
+        int L = in.nextInt();
+        int H = in.nextInt();
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+
+        String T = in.nextLine();
+        for (int i = 0; i < H; i++) {
+            String ROW = in.nextLine();
+        }
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("answer");
+    }
+}
